0d895e46d8a83117c6f1622707db1270cabf6405
Eric <eric.halonen1@gmail.com>
2020-09-14 14:48:16 -0400

    setUp files, typescript and src added

---
+++ src/ChatForum.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+export interface thread {
+    multiMessages: string[]
+    kingMessage: string
+
+}
+interface threads {
+    thread: thread[]
+}
+//list of kingMessages, onCLick=> show multiMessages
+export default function chatForum() {
+
+}
